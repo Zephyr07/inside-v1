91ac4a2c238492a3c0b8f38d20ca1adcfcac0ad5
Edward NANDA <enanda52@gmail.com>
2021-09-10 00:06:34 +0100

    Utilisateur : - Modification page d'acceuil ok

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ApiProvider} from "../../providers/api/api";
 import * as _ from "lodash";
+import {ModalDismissReasons, NgbModal} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-home',
@@ -8,12 +9,17 @@ import * as _ from "lodash";
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  private user :any;
+  public user :any;
   throttle = 30;
   scrollDistance = 1;
   scrollUpDistance = 2;
   page = 1;
+  max_length = 0;
+  old_max_length = 0;
+  last_page = 10000000;
   public post :any = [];
+  public posts :any = [];
+  public show_more_post = false;
   public show_post = true;
   public anniv :any = [];
   public show_anniv = true;
@@ -23,7 +29,14 @@ export class HomeComponent implements OnInit {
   public note :any = [];
   public show_note = true;
   public entity = "";
+  public show = false;
+  public imageSrc ="";
+  public closeResult ="";
+  public content ="";
+  private file_selected = false;
+  public image = new FormData();
   constructor(
+    private modalService: NgbModal,
     private api : ApiProvider
   ) {
     // @ts-ignore
@@ -31,7 +44,7 @@ export class HomeComponent implements OnInit {
     this.getBirthday();
     this.getGroup();
     this.getNote();
-    this.getPost();
+    this.getPosts(true);
   }
 
   ngOnInit(): void {
@@ -76,7 +89,8 @@ export class HomeComponent implements OnInit {
       this.entity = "de "+a.body.entity.name;
       let opt = {
         per_page:10,
-        _sort:'updated_at',
+        _sort:'date',
+        _sortDir:'desc',
         entity_id: a.body.entity_id,
         _includes: 'newsletter'
       };
@@ -89,6 +103,8 @@ export class HomeComponent implements OnInit {
             this.note.push(v);
           }
         });
+        this.events = _.orderBy(this.events,'date').reverse();
+        this.note = _.orderBy(this.note,'date').reverse();
         this.show_note = false;
       }, (e:any)=>{
         console.log(e);
@@ -98,34 +114,158 @@ export class HomeComponent implements OnInit {
     });
   }
 
-  getPost(){
-    const opt = {
-      per_page:10,
-      _sort:'created_at',
-      _sortDir:'desc',
-      _includes:'employee'
-    };
-    this.api.Posts.getList(opt).subscribe((p:any)=>{
-      p.forEach((v:any)=>{
-        if(v.post_id==undefined || v.post_id==null){
-          this.post.push(v);
-        }
-        v.comment = _.filter(p,{post_id:v.id}).length;
-      });
-      this.show_post = false;
-      //console.log(p);
-      //this.post = p;
+  getPosts(s:boolean){
+    if(s){
+      // première requète
+      this.page = 1;
+    } else {
+      this.show_more_post = true;
+    }
+    if(this.page <= this.last_page){
+      const opt = {
+        per_page:10,
+        'post_id-nl':true,
+        _sort:'created_at',
+        _sortDir:'desc',
+        _includes:'employee,posts.employee,ratings',
+        page: this.page
+      };
+      this.api.Posts.getList(opt).subscribe((p:any)=>{
+        this.last_page = p.metadata.last_page;
+        this.max_length = p.metadata.total;
+        this.old_max_length = this.max_length;
+        p.forEach((v:any)=>{
+          if(v.post_id==undefined || v.post_id==null){
+            v.posts = _.orderBy(v.posts, 'updated_at').reverse();
+            v.comment = _.filter(p,{post_id:v.id}).length;
+            p.commentaire = "";
+            v.like = _.find(v.ratings, {employee_id: this.user.employee.id}) != undefined;
+            v.show = false;
+            this.posts.push(v);
+          }
+        });
+        this.posts = _.orderBy(this.posts, 'updated_at').reverse();
+        this.show_more_post = false;
+        this.show_post = false;
+        this.page++;
+      })
+    } else {
+      this.show_more_post = false;
+    }
+
+  }
+
+  savePost(){
+    this.show = true;
+    this.api.Posts.post({content:this.content,employee_id:this.user.employee.id}).subscribe((e:any)=>{
+      // enretistrement de l'image
+      if(this.file_selected){
+        this.image.append('_method', 'PUT');
+        this.api.restangular.all('posts/' + e.body.id).customPOST(this.image, undefined, undefined, {'Content-Type': undefined}).subscribe((d:any) => {
+          //console.log('ok', d);
+          alert("Publié");
+          this.show = false;
+          this.content = "";
+          this.imageSrc = "";
+          this.image = new FormData();
+        }, (e:any)=>{
+          console.log(e);
+          this.show = false;
+        });
+      } else {
+        // pas d'image on ne fait rien
+        this.show = false;
+      }
     })
   }
 
-  trackByIndex(index: number, obj: any): any {
-    return index;
+  saveComment(p:any){
+    p.show = true;
+    const opt = {
+      content : p.commentaire,
+      employee_id: this.user.employee.id,
+      post_id: p.id
+    };
+    this.api.Posts.post(opt).subscribe((d:any)=>{
+      d.body.employee = this.user.employee;
+      p.posts.push(d.body);
+      p.posts = _.orderBy(p.posts,'updated_at').reverse();
+      p.commentaire = "";
+      p.show = false;
+    }, (e:any)=>{
+      console.log(e);
+      p.show = false;
+    })
+
   }
 
-  onScrollDown(ev:any) {
-    //this.getBills(false);
+  deleteComment(post:any,p:any){
+    const x = _.find(post.posts,p);
+    post.posts.splice(post.posts.indexOf(x),1);
+    this.api.Posts.get(p.id).subscribe((e:any)=>{
+      e.id = e.body.id;
+      e.remove();
+    }, (e:any)=>{
+      console.log(e);
+      p.show = false;
+    })
+  };
+
+  saveRating(p:any){
+    if(!p.like){
+      // ajout
+      p.like = true;
+      p.ratings.push({id:0,employee_id:this.user.employee.id,post_id:p.id});
+      this.api.Ratings.post({employee_id:this.user.employee.id,post_id:p.id}).subscribe((d:any)=>{
+
+      }, (e:any)=>{
+        console.log(e);
+      })
+    } else {
+      p.like = false;
+      const x = _.find(p.ratings,{employee_id:this.user.employee.id,post_id:p.id});
+      p.ratings.splice(p.ratings.indexOf(x),1);
+      //suppression
+      this.api.Ratings.getList({employee_id:this.user.employee.id,post_id:p.id}).subscribe((v:any)=>{
+        v[0].remove().subscribe(()=>{
+        }, (e:any)=>{
+          console.log(e);
+        } )
+      })
+    }
   }
 
-  onUp(ev:any) {
+  onSelectFile(event:any) {
+    this.file_selected = true;
+    const reader = new FileReader();
+    reader.readAsDataURL(event.target.files[0]);
+    //this.imageSrc = reader.result as string;
+    this.image.append('image', event.target.files[0], event.target.files[0].name);
+    reader.onload = () => {
+      this.imageSrc = reader.result as string;
+    };
+  }
+
+  openModal(){
+    // @ts-ignore
+    document.getElementById('btnModal').click();
+  }
+
+  open(content:any) {
+    this.modalService.open(content, {centered: true,scrollable: true}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 }
