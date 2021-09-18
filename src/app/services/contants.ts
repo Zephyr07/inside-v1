4196d0c079635925280940bb7f4707140916167d
Edward NANDA <enanda52@gmail.com>
2021-09-18 20:22:09 +0100

    integration lien aws

--- src/app/services/contants.ts
+++ src/app/services/contants.ts
@@ -1,3 +1,3 @@
 //export const API_ENDPOINT = 'https://api-izis.bvs-sas.com/api/';
-// export const API_ENDPOINT = 'http://comrec-dev.gbmvzg36dp.us-east-1.elasticbeanstalk.com/api/';
- export const API_ENDPOINT = 'http://inside.ds.local/api/';
+ export const API_ENDPOINT = 'http://inside-env.eba-sjrkv3t2.us-east-1.elasticbeanstalk.com/api/';
+ // export const API_ENDPOINT = 'http://inside.ds.local/api/';
